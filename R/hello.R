2e73ff669b3ea30ad2e0695d1e8c091fca306d05
Neal Fultz <nfultz@gmail.com>
2017-08-25 18:37:25 -0700

    shitty service proto parser

--- R/hello.R
+++ R/hello.R
@@ -17,3 +17,69 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+read_services <- function(file){
+  SERVICE = "service"
+  RPC = "rpc"
+  RETURNS = "returns"
+  STREAM = "stream"
+
+  services <- list()
+
+  doServices <- function(i){
+    service_name <- tokens[i+1]
+    services[[service_name]] <<- list()
+
+    while(tokens[i] != '}') {
+      if(tokens[i] == RPC){
+        i <- doRPC(i, service_name)
+      }
+      i <- i + 1
+    }
+
+    return(i)
+  }
+
+  doRPC <- function(i, service_name) {
+    rpc_name = tokens[i+1]
+    fn <- I
+
+    w <- "RequestType"
+
+    while(tokens[i] != '}'){
+
+      if(tokens[i] == '('){
+        i <- i + 1
+        isStream <- tokens[i] == STREAM
+        if(isStream){
+          i <- i + 1
+        }
+
+        attributes(fn)[[w]] <- list(name=tokens[i], stream=isStream)
+        w <- "ResponseType"
+      }
+
+      i <- i + 1
+    }
+    services[[service_name]][[rpc_name]] <<- fn
+    return(i)
+  }
+
+
+  lines <- readLines(file)
+
+  tokens <- strsplit(lines, '(^//.*$|\\s+|(?=[{}();]))', perl=TRUE) %>% unlist %>% Filter(f=nchar)
+
+  i <- 1
+  while(i <= length(tokens)){
+    if(tokens[i] == SERVICE){
+      i <- doServices(i)
+    }
+
+    i <- i + 1
+  }
+
+  services
+}
+
+
