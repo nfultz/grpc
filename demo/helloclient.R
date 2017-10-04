15af14b7b3ae5c9723c0d6e6b4257c953aa541ba
Neal Fultz <nfultz@gmail.com>
2017-10-04 18:17:49 +0000

    clean up demos

--- demo/helloclient.R
+++ demo/helloclient.R
@@ -1,15 +1,15 @@
-helloclient <- function(){
-  
-  
-  spec <- system.file('examples/helloworld.proto', package = 'grpc')  
-  impl <- read_services(spec)
-  client <- grpc_client(impl, "localhost:50051")
-  
-  neal = client$SayHello$build(name="Neal")
-  
-  hello <- client$SayHello$call(neal)
-  
-  print(hello)
-  print(as.list(hello))
-  
-}
+#' Example gRPC client
+#'
+#' Reads a message with a name and returns a message greeting the name.
+#' @references \url{https://github.com/grpc/grpc/tree/master/examples/cpp/helloworld}
+
+spec <- system.file('examples/helloworld.proto', package = 'grpc')  
+impl <- read_services(spec)
+client <- grpc_client(impl, "localhost:50051")
+
+neal = client$SayHello$build(name="Neal")
+
+hello <- client$SayHello$call(neal)
+
+print(hello)
+print(as.list(hello))
--- demo/helloserver.R
+++ demo/helloserver.R
@@ -2,20 +2,17 @@
 #'
 #' Reads a message with a name and returns a message greeting the name.
 #' @references \url{https://github.com/grpc/grpc/tree/master/examples/cpp/helloworld}
-helloworld <- function() {
 
-    ## reading the service definitions
-    spec <- system.file('examples/helloworld.proto', package = 'grpc')
-    
-    impl <- read_services(spec)
-  
-    impl$SayHello$f <- function(request){
-      newResponse(message = paste('Hello,', request$name))
-    }
-    
+## reading the service definitions
+spec <- system.file('examples/helloworld.proto', package = 'grpc')
 
-    ## actually running the service handlers
-    start_server(impl, "0.0.0.0:50051")
+impl <- read_services(spec)
 
+impl$SayHello$f <- function(request){
+  newResponse(message = paste('Hello,', request$name))
 }
+
+
+## actually running the service handlers
+start_server(impl, "0.0.0.0:50051")
 
