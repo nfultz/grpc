87cd71b18f57083b00f224a21f3d8aa0cba0689d
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2017-10-05 16:19:27 +0200

    (re)add server and client for iris scoring

---
+++ demo/iris-client.R
@@ -0,0 +1,23 @@
+## Example gRPC client to score features sent in a Proto message
+
+library(grpc)
+library(RProtoBuf)
+
+## reading the service definitions
+spec <- system.file('examples/iris_classifier.proto', package = 'grpc')
+impl <- read_services(spec)
+
+## connection to the gRPC server
+client <- grpc_client(impl, 'localhost:50051')
+
+## define message to be sent to the gRPC server
+msg <- client$Classify$build(Sepal_Length = 5, Sepal_Width = 2, Petal_Length = 1, Petal_Width = 0.3)
+
+## score
+res <- client$Classify$call(msg)
+
+str(res)
+
+## log results
+library(futile.logger)
+flog.info('Result: ', res$Species)
