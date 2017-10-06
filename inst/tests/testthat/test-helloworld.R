fe562d1e385959c3f15a945311f318f1549aa479
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2017-10-06 15:17:24 +0200

    fix context name

--- inst/tests/testthat/test-helloworld.R
+++ inst/tests/testthat/test-helloworld.R
@@ -2,16 +2,16 @@ library(testthat)
 library(grpc)
 library(parallel)
 
+context('helloworld example')
+
 ## start a server in the background
 p <- parallel:::mcfork()
 parallel:::sendChildStdin(p, "demo('helloserver', 'grpc', ask = FALSE)\n")
 
-context('iris')
 test_that('server started', {
     expect_true(inherits(p, 'process'))
 })
 
-context('client')
 test_that('using client', {
     expect_true(any(
         grepl('Hello, Neal',
