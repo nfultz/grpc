6305288f477a8d8ea853fd655b2f6729e304d2a5
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2017-10-06 13:59:25 +0200

    remove Rcpp boilerplate

--- src/rcpp_hello.cpp
+++ src/rcpp_hello.cpp
@@ -3,19 +3,6 @@
 
 using namespace Rcpp;
 
-// This is a simple function using Rcpp that creates an R list
-// containing a character vector and a numeric vector.
-//
-// Learn more about how to use Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//
-// and browse examples of code using Rcpp at:
-//
-//   http://gallery.rcpp.org/
-//
-
 //' @export
 // [[Rcpp::export]]
 CharacterVector grpc_version() {
