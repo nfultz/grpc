8dcd19acd84cccb4f4e0ab23d865fabfd2c40649
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2017-10-07 01:21:09 +0200

    not just start, but wait for the server to start

--- inst/tests/testthat/test-helloworld.R
+++ inst/tests/testthat/test-helloworld.R
@@ -6,13 +6,23 @@ library(processx)
 context('helloworld example')
 
 ## start a server in the background
-p <- process$new(commandline = "R -e \"demo('helloserver', 'grpc', ask = FALSE)\"")
+p <- process$new(commandline = "R -e \"demo('helloserver', 'grpc', ask = FALSE)\"", stdout = "|", stderr = "|")
 
 test_that('server started', {
     expect_true(p$is_alive())
 })
 
-## Sys.sleep(5)
+## wait for the server to start
+while (TRUE) {
+    Sys.sleep(1)
+    if (any(p$read_output_lines() == "Entering Event Loop")) {
+        break()
+    }
+}
+
+test_that('server running', {
+    expect_true(p$is_alive())
+})
 
 test_that('using client', {
     expect_true(any(
