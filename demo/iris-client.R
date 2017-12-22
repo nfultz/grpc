48759bf12f0b42fe721bb1897889aef8fa44f9af
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2017-12-22 17:21:55 +0100

    ~batch scoring

--- demo/iris-client.R
+++ demo/iris-client.R
@@ -2,6 +2,7 @@
 
 library(grpc)
 library(RProtoBuf)
+library(futile.logger)
 
 ## reading the service definitions
 spec <- system.file('examples/iris_classifier.proto', package = 'grpc')
@@ -16,8 +17,25 @@ msg <- client$Classify$build(Sepal_Length = 5, Sepal_Width = 2, Petal_Length = 1
 ## score
 res <- client$Classify$call(msg)
 
-str(res)
-
 ## log results
-library(futile.logger)
 flog.info('Result: %s with %s probability', res$Species, res$Probability)
+
+## score a number of items
+df <- iris[sample(1:150, 25), ]
+
+for (n in seq_len(nrow(df))) {
+
+    ## define message to be sent to the gRPC server
+    msg <- client$Classify$build(
+                               Sepal_Length = df[n, 'Sepal.Length'],
+                               Sepal_Width  = df[n, 'Sepal.Width'],
+                               Petal_Length = df[n, 'Petal.Length'],
+                               Petal_Width  = df[n, 'Petal.Width'])
+
+    ## score
+    res <- client$Classify$call(msg)
+
+    ## log results
+    flog.info('Result: %s with %s probability', res$Species, res$Probability)
+
+}
