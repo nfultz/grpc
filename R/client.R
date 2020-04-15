7cae81812eaaaa9a897ba17fddd0d24bd4b87942
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-04-15 22:48:50 +0200

    Added an argument client_deadline
    This argument will be used to manually configure the deadline for client timeout. It will be helpful for those RPCs that take longer than 5 seconds.

--- R/client.R
+++ R/client.R
@@ -5,7 +5,7 @@
 #' @return client handle
 #' @importFrom RProtoBuf P serialize read new
 #' @export
-grpc_client <- function(impl, channel) {
+grpc_client <- function(impl, channel, client_deadline) {
   
   
   client_functions <- lapply(impl, function(fn)
@@ -14,7 +14,7 @@ grpc_client <- function(impl, channel) {
       ResponseDescriptor <- P(fn[["ResponseType"]]$proto)
       
       list(
-        call = function(x, metadata=character(0)) read(ResponseDescriptor, fetch(channel, fn$name, serialize(x, NULL), metadata)),
+        call = function(x, metadata=character(0)) read(ResponseDescriptor, fetch(channel, fn$name, serialize(x, NULL), metadata, client_deadline)),
         build = function(...) new(RequestDescriptor, ...)
       )
     })
