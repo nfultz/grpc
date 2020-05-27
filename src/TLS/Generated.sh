bbc62d7d9c9a2850607dc23500ccc7d8cf7e73bb
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-05-27 16:48:32 +0200

    Upload files to generate TLS certificates

---
+++ src/TLS/Generated.sh
@@ -0,0 +1,11 @@
+rm *.pem
+
+# 1. Generate CA's private key and self-signed certificate
+openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=EG/ST=CairoEG/L=Cairo/O=GSoC/OU=CerAuth/CN=localhost/emailAddress=ca@gmail.com"
+
+# 2. Generate web server's private key and certificate signing request (CSR)
+openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=EG/ST=AlexEG/L=Alex/O=GSoC/OU=Server/CN=localhost/emailAddress=server@gmail.com"
+
+# 3. Use CA's private key to sign web server's CSR and get back the signed certificate
+openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
+
