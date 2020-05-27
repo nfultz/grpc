f90468ce137105237154b007b3d63d1f38858dce
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-05-27 04:58:27 +0200

    Added header files

---
+++ src/Server_Libraries.h
@@ -0,0 +1,18 @@
+#ifndef SERVERLIBRARIES_H
+#define SERVERLIBRARIES_H
+
+#include <Rcpp.h>
+#include <grpc/grpc.h>
+#include <grpc/impl/codegen/byte_buffer_reader.h>
+#include <grpc++/grpc++.h>
+#include <string>
+#include <grpc/slice.h>
+#include <grpc/grpc_security.h>
+#include <grpc/support/alloc.h>
+#include <grpc/support/string_util.h>
+#include <vector>
+#include "common.h"
+
+#define _INTERRUPT_CHECK_PERIOD_MS 1000
+
+#endif
