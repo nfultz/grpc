e26c5555d7f1aafd7a6cbf3fc51ca64e3e587e77
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-06-03 00:54:02 +0200

    Update caches.js

--- .github/configs/caches.js
+++ .github/configs/caches.js
@@ -3,9 +3,24 @@ module.exports = {
   openssl: 
   {
     path: [`${process.env.HOME}/.cache/openssl`],
-    hashFiles: ['requirements*.txt'],
+    hashFiles: ['openssl.txt'],
     keyPrefix: 'openssl-',
     restoreKeys: 'openssl-',
   },
   
+  proto: 
+  {
+    path: [`${process.env.HOME}/.cache/proto`],
+    hashFiles: ['proto.txt'],
+    keyPrefix: 'proto-',
+    restoreKeys: 'proto-',
+  },
+  
+  grpc_base: 
+  {
+    path: [`${process.env.HOME}/.cache/grpc_base`],
+    hashFiles: ['grpc_base.txt'],
+    keyPrefix: 'grpc_base-',
+    restoreKeys: 'grpc_base-',
+  },
 }
