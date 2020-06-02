b6ce09e89f32af56befc4f966065214f9ed40ee5
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-06-03 00:49:50 +0200

    Update caches.js

--- .github/configs/caches.js
+++ .github/configs/caches.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+  
+  openssl: 
+  {
+    path: [`${process.env.HOME}/.cache/openssl`],
+    hashFiles: ['requirements*.txt'],
+    keyPrefix: 'openssl-',
+    restoreKeys: 'openssl-',
+  },
+  
+}
