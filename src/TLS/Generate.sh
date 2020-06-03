3f7244a42232164be11525c1da35105892f5a5dc
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-06-03 15:04:05 +0200

    Commands to generate Client Certificates

--- src/TLS/Generate.sh
+++ src/TLS/Generate.sh
@@ -9,3 +9,8 @@ openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
 openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
 
+# 4. Generate client's private key and certificate signing request (CSR)
+openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -subj "/C=FR/ST=Alsace/L=Strasbourg/O=PC Client/OU=Computer/CN=localhost/emailAddress=pcclient@gmail.com"
+
+# 5. Use CA's private key to sign client's CSR and get back the signed certificate
+openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
