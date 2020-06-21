f06b5727af1639bbc07022696a9ead590a8c808d
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-06-21 18:36:54 +0200

    Added authentication functionality

--- R/client.R
+++ R/client.R
@@ -1,28 +1,41 @@
-#' Build a client handle
-#' 
+#' Build a client handle 
 #' @param stubs the stub of the service
 #' @param channel what to connect to
 #' @return client handle
 #' @importFrom RProtoBuf P serialize read new
 #' @export
-grpc_client <- function(impl, channel) {
-
-  client_deadline <- getOption("client_deadline", default = 5)
+grpc_client <- function(impl, channel)
+{
+  
   UseTLS <- getOption("UseTLS", default = TRUE)
   CertPath <- getOption("CertPath", default = NULL)
-  
-  client_functions <- lapply(impl, function(fn)
+  AccessToken <- getOption("AccessToken", default = NULL)
+  ClientDeadline <- getOption("ClientDeadline", default = 5)
+
+  # lapply function is applied for operations on list objects and 
+  # returns a list object of same length of original set.
+  client_functions <- lapply(impl, 
+    function(fn)
     {
-      RequestDescriptor <- P(fn[["RequestType"]]$proto)
-      ResponseDescriptor <- P(fn[["ResponseType"]]$proto)
-      
-      list(
-        call = function(x, metadata=character(0)) read(ResponseDescriptor, fetch(channel, fn$name, serialize(x, NULL), metadata, client_deadline, UseTLS, CertPath)),
-        build = function(...) new(RequestDescriptor, ...)
-      )
-    })
-  
-  
+        RequestDescriptor   <- P(fn[["RequestType"]]$proto)
+        ResponseDescriptor  <- P(fn[["ResponseType"]]$proto)
+
+        list(
+            call = function(x, metadata=character(0)) 
+            {   
+                read(
+                    ResponseDescriptor, 
+                    fetch(channel, fn$name, serialize(x, NULL), metadata, UseTLS, CertPath, AccessToken, ClientDeadline)
+                    )
+            },
+            
+            build = function(...) 
+            {
+              new(RequestDescriptor, ...)
+            }
+        )
+    }
+  )
   
   client_functions
 }
