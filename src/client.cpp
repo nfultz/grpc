ca38e5ef9458a72f320813b68ef3482ab2a7b5f0
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-06-21 18:33:12 +0200

    Clean Code + Authentication
    Added authentication functionality
    Removed dead code
    Renamed variables
    Introduced some modular functions

--- src/client.cpp
+++ src/client.cpp
@@ -4,9 +4,11 @@
 
 using namespace Rcpp;
 
-static void *tag(intptr_t i) { return (void *)i; }
+static void* tag(intptr_t i) {
+  return (void*)i;
+}
 
-RawVector sliceToRaw2(grpc_slice slice){
+RawVector sliceToRaw2(grpc_slice slice) {
 
   int n = GRPC_SLICE_LENGTH(slice);
 
@@ -17,13 +19,11 @@ RawVector sliceToRaw2(grpc_slice slice){
   RawVector out(n);
   for(int i = 0; i < n; i++)
     out[i] = (unsigned char) data[i];
-  // std::copy(data, data+n, out.begin());
 
   return out;
 }
 
-static std::string get_file_contents(const char *fpath)
-{
+static std::string get_file_contents(const char *fpath) {
   std::ifstream finstream(fpath);
 
   std::string contents(
@@ -34,80 +34,106 @@ static std::string get_file_contents(const char *fpath)
   return contents;
 }
 
-grpc_channel_credentials* Get_Client_Credentials(const char* path){
+const char* getOauth2AccessToken(const char* tokenValue) {
 
-  std::string ca_cert_pem = get_file_contents(((std::string)path + "ca-cert.pem").c_str());
-  std::string client_key_pem = get_file_contents(((std::string)path + "client-key.pem").c_str());
-  std::string client_cert_pem = get_file_contents(((std::string)path + "client-cert.pem").c_str());
+  if(tokenValue == nullptr)
+    stop("Access token value is not defined");
 
+  const char* accessToken(tokenValue + sizeof("Bearer ") - 1);
+  
+  return accessToken;
+}
+
+grpc_channel_credentials* getClientCredentials(const char* path, const char* tokenValue, bool isMetadataAttached){
+
+  files PEMfiles;
+
+  PEMfiles.CAcert = 
+    get_file_contents(((std::string)path + "ca-cert.pem").c_str());
+  PEMfiles.clientKey = 
+    get_file_contents(((std::string)path + "client-key.pem").c_str());
+  PEMfiles.clientCert = 
+    get_file_contents(((std::string)path + "client-cert.pem").c_str());
+  
   grpc_ssl_pem_key_cert_pair signed_client_key_cert_pair =
-    {client_key_pem.c_str(), client_cert_pem.c_str()};
+    {(PEMfiles.clientKey).c_str(), (PEMfiles.clientCert).c_str()};
 
-  grpc_channel_credentials* Creds = grpc_ssl_credentials_create(
-    ca_cert_pem.c_str(), &signed_client_key_cert_pair, nullptr, nullptr);
+  grpc_channel_credentials* creds = grpc_ssl_credentials_create(
+    (PEMfiles.CAcert).c_str(), &signed_client_key_cert_pair, nullptr, nullptr);
 
-  return Creds;
+  if(isMetadataAttached)
+    return creds;
+
+  grpc_call_credentials* oauth2Creds = 
+    grpc_access_token_credentials_create(getOauth2AccessToken(tokenValue), nullptr);
+  
+  grpc_channel_credentials* credsTLSOauth =
+    grpc_composite_channel_credentials_create(creds, oauth2Creds, nullptr);
+    
+  return credsTLSOauth;
+}
+
+grpc_channel* createChannel(bool useTLS, const char* server ,const char* path, 
+                            const char* tokenValue, bool isMetadataAttached) {
+  
+  grpc_channel* channel;
+
+  if(useTLS) {
+    grpc_channel_credentials* client_creds = getClientCredentials(path, tokenValue, isMetadataAttached);
+    channel = grpc_secure_channel_create(client_creds, server, NULL, nullptr);
+  }
+  
+  else {
+    channel = grpc_insecure_channel_create(server, NULL, nullptr);
+  }
+
+  return channel;
 }
 
 // [[Rcpp::export]]
 RawVector fetch(CharacterVector server, CharacterVector method, RawVector requestArg, CharacterVector metadata, 
-                int client_deadline, bool UseTLS, CharacterVector CertPath) {
-  
-  // gpr_timespec deadline = five_seconds_from_now();
-  
-  
-  //setup
-  grpc_call *c;
-  
-  grpc_completion_queue *cq = grpc_completion_queue_create_for_next(NULL);
-  
-  const grpc_slice server_slice = grpc_slice_from_copied_string(server[0]);
-  const grpc_slice method_slice = grpc_slice_from_copied_string(method[0]);
+                bool useTLS, CharacterVector certPath, CharacterVector tokenValue, int clientDeadline) {
 
-  SEXP raw_ = requestArg;
-  int len = requestArg.length();
-  grpc_slice request_payload_slice = grpc_slice_from_copied_buffer((char*) RAW(raw_), len);
+  bool isMetadataAttached = 
+    (metadata.length() > 0) ? true : false;
+
+  grpc_channel* channel = 
+    createChannel(useTLS, server[0], certPath[0], tokenValue[0], isMetadataAttached);
+
+  grpc_completion_queue* cq = 
+    grpc_completion_queue_create_for_next(nullptr);
+
+  const grpc_slice method_slice = 
+    grpc_slice_from_copied_string(method[0]);
   
-  
+  const grpc_slice server_slice = 
+    grpc_slice_from_copied_string(server[0]);
   const grpc_slice *sp = &server_slice;
-  
-  grpc_channel* channel;
 
-  if(UseTLS) {
-    grpc_channel_credentials* client_creds = Get_Client_Credentials(CertPath[0]);
-    channel = grpc_secure_channel_create(client_creds, server[0], NULL, nullptr);
-    grpc_channel_credentials_release(client_creds);
-  }
+  //Get client deadline in milliseconds
+  int timeout = clientDeadline * 1000;
+  gpr_timespec deadline =
+    gpr_time_add(gpr_now(GPR_CLOCK_REALTIME), gpr_time_from_millis(timeout, GPR_TIMESPAN));
 
-  else {
-    channel = grpc_insecure_channel_create(server[0], NULL, nullptr);
-  }
-  
-  gpr_timespec deadline = gpr_time_add(gpr_now(GPR_CLOCK_REALTIME), gpr_time_from_millis(client_deadline*1000, GPR_TIMESPAN));
-  
   RGRPC_LOG("Create Call");
-  c = grpc_channel_create_call(
-    channel, NULL, GRPC_PROPAGATE_DEFAULTS, cq,
-    method_slice, //grpc_slice_from_static_string("/foo"),
-    sp, 
-    deadline,
-    NULL);
-  
-  
-  RGRPC_LOG("Making ops");  
-  // perform request
+  grpc_call* c = grpc_channel_create_call(
+                      channel, NULL, GRPC_PROPAGATE_DEFAULTS, 
+                      cq, method_slice, sp, deadline, NULL);
+
+  RGRPC_LOG("Making ops");
   grpc_op ops[6];
-  grpc_op *op;
+  grpc_op* op;
 
   int metadata_length = metadata.length() / 2;
   grpc_metadata meta_c[metadata_length];
 
   for(int i = 0; i < metadata_length; i++) {
     meta_c[i] = {grpc_slice_from_static_string(metadata[i * 2]),
-        grpc_slice_from_static_string(metadata[i * 2 + 1]),
-        0,
-        {{nullptr, nullptr, nullptr, nullptr}}};
+                 grpc_slice_from_static_string(metadata[i * 2 + 1]),
+                 0,
+                 {{nullptr, nullptr, nullptr, nullptr}}};
   }
+
 
   grpc_metadata_array initial_metadata_recv;
   grpc_metadata_array trailing_metadata_recv;
@@ -118,51 +144,69 @@ RawVector fetch(CharacterVector server, CharacterVector method, RawVector reques
   grpc_call_error error;
   grpc_event event;
   grpc_slice details;
-  int was_cancelled = 2;
-  
-  
-  
+
   grpc_metadata_array_init(&initial_metadata_recv);
   grpc_metadata_array_init(&trailing_metadata_recv);
   grpc_metadata_array_init(&request_metadata_recv);
   grpc_call_details_init(&call_details);
-  
-  
+
+
+  SEXP raw_ = requestArg;
+  int len = requestArg.length();
+  grpc_slice request_payload_slice = grpc_slice_from_copied_buffer((char*) RAW(raw_), len);
+
   grpc_byte_buffer *request_payload =
     grpc_raw_byte_buffer_create(&request_payload_slice, 1);
-  
+
+
   memset(ops, 0, sizeof(ops));
+  
+  // 1
   op = ops;
   op->op = GRPC_OP_SEND_INITIAL_METADATA;
   op->data.send_initial_metadata.count = metadata_length;
+  
   if (metadata_length > 0) {
     op->data.send_initial_metadata.metadata = meta_c;
+    RGRPC_LOG("Initial Metadata = " << meta_c)
   }
+
   else {
-    op->data.send_initial_metadata.maybe_compression_level.is_set = false;    
+    op->data.send_initial_metadata.maybe_compression_level.is_set = false;
   }
+
   op->flags = 0;
   op->reserved = NULL;
   op++;
+
+  // 2
   op->op = GRPC_OP_SEND_MESSAGE;
   op->data.send_message.send_message = request_payload;
   op->flags = 0;
   op->reserved = NULL;
   op++;
+
+  // 3
   op->op = GRPC_OP_SEND_CLOSE_FROM_CLIENT;
   op->flags = 0;
   op->reserved = NULL;
   op++;
+
+  // 4
   op->op = GRPC_OP_RECV_INITIAL_METADATA;
   op->data.recv_initial_metadata.recv_initial_metadata = &initial_metadata_recv;
   op->flags = 0;
   op->reserved = NULL;
   op++;
+
+  // 5
   op->op = GRPC_OP_RECV_MESSAGE;
   op->data.recv_message.recv_message = &response_payload_recv;
   op->flags = 0;
   op->reserved = NULL;
   op++;
+
+  // 6
   op->op = GRPC_OP_RECV_STATUS_ON_CLIENT;
   op->data.recv_status_on_client.trailing_metadata = &trailing_metadata_recv;
   op->data.recv_status_on_client.status = &status;
@@ -170,41 +214,40 @@ RawVector fetch(CharacterVector server, CharacterVector method, RawVector reques
   op->flags = 0;
   op->reserved = NULL;
   op++;
-  
+
   RGRPC_LOG("Starting batch...");
+
   error = grpc_call_start_batch(c, ops, (size_t)(op - ops), tag(1), NULL);
   if (error != GRPC_CALL_OK) { // 0
     stop("gRPC c++ call start failed");
   }
-  event = grpc_completion_queue_next(cq, deadline, RESERVED); //actually does the work
+
+  event = grpc_completion_queue_next(cq, deadline, nullptr); //actually does the work
   if (event.type == GRPC_QUEUE_TIMEOUT) { // 1
     stop("gRPC c++ call timeout");
   }
+
   if (event.success == 0) {
     stop("gRPC c++ call error");
   }
+
+  if(status == GRPC_STATUS_UNAUTHENTICATED) {
+    stop("The request does not have valid authentication credentials");
+  }
+
   if (!response_payload_recv) {
     stop("No response from the gRPC server");
   }
 
-  // client_batch[grpc.opType.SEND_INITIAL_METADATA] =
-  //   metadata._getCoreRepresentation();
-  // client_batch[grpc.opType.SEND_MESSAGE] = message;
-  // client_batch[grpc.opType.SEND_CLOSE_FROM_CLIENT] = true;
-  // client_batch[grpc.opType.RECV_INITIAL_METADATA] = true;
-  // client_batch[grpc.opType.RECV_MESSAGE] = true;
-  // client_batch[grpc.opType.RECV_STATUS_ON_CLIENT] = true;
-  
-  
-  
-  
+
   // Call Header, followed by optional Initial-Metadata, followed by zero or more Payload Messages
   RGRPC_LOG("Read response Slice");
   grpc_byte_buffer_reader bbr;
-   
+
   try {
     grpc_byte_buffer_reader_init(&bbr, response_payload_recv);
   }
+
   catch (std::exception& e) {
     Rcout << "Segfault" << e.what() << std::endl;
     stop("Segfault in C++");
@@ -212,34 +255,24 @@ RawVector fetch(CharacterVector server, CharacterVector method, RawVector reques
 
   grpc_slice response_payload_slice = grpc_byte_buffer_reader_readall(&bbr);
   RawVector response_payload_raw = sliceToRaw2(response_payload_slice);
-  
+
+
   RGRPC_LOG("Cleanup");
-  
-  // teardown
+
   grpc_completion_queue_shutdown(cq);
-  //drain_cq(cq);
   grpc_completion_queue_destroy(cq);
   cq = NULL;
   grpc_channel_destroy(channel);
   channel = NULL;
-  //gpr_free(cf->server_uri);
-  
-  
+
   grpc_slice_unref(details);
   grpc_metadata_array_destroy(&initial_metadata_recv);
   grpc_metadata_array_destroy(&trailing_metadata_recv);
   grpc_metadata_array_destroy(&request_metadata_recv);
   grpc_call_details_destroy(&call_details);
-  
-  // grpc_call_unref(c);
-  // grpc_call_unref(s);
-  // 
-  // cq_verifier_destroy(cqv);
-  
+
   grpc_byte_buffer_destroy(request_payload);
   grpc_byte_buffer_destroy(response_payload_recv);
-  
 
   return response_payload_raw;
-  
 }
